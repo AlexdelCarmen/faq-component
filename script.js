49206268597b01bf5dfcd3b77d927de604ba6abc
Cyrborg Werewolf <alexbdelcarmen@gmail.com>
2022-08-25 22:18:02 -0600

    Create hide/show effect for FAQ answers.

--- script.js
+++ script.js
@@ -4,5 +4,6 @@ const allQuestions = document.querySelectorAll(".faq-question");
 allQuestions.forEach((question) =>
   question.addEventListener("click", () => {
     question.childNodes[1].classList.toggle("flip");
+    document.getElementById(question.dataset.code).classList.toggle("visible");
   })
 );
